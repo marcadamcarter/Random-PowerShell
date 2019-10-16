9aa9af3c95b77df07a3c2ff18be31ec40bec2cdb
marc carter <marc.a.carter@gmail.com>
2019-10-16 13:00:24 -0500

    Initial commit
    Displays a list of hyperlinks from a web page.
    Useful for identifying absolute hyperlinks when preparing for content migration

---
+++ SharePoint/Get-PageLinks.ps1
@@ -0,0 +1,42 @@
+<#
+	.SYNOPSIS
+		Displays a list of hyperlinks from a web page.
+	
+	.DESCRIPTION
+        Useful for identifying absolute hyperlinks when preparing for content migration
+
+    .EXAMPLE
+        Get-PageLinks -URL https://www.nist.gov/about-nist
+        Get-PageLinks -URL https://www.army.mil | sort protocol | where protocol -notlike "javascript*"
+        "https://www.nist.gov/about-nist" | Get-PageLinks | where protocol -like "http*"
+#>
+function Get-PageLinks{
+    [CmdletBinding()]
+    Param( 
+        # URL of the web page to query
+        [Parameter(Mandatory=$true, Position=0,ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string[]]$URL
+    )
+    Begin { }
+    Process { 
+        foreach ($address in  $URL){
+            $array = @()
+            $wr = Invoke-WebRequest -Uri $address -Method Get -UseDefaultCredentials
+            $links = $wr.ParsedHtml.links
+            Write-Host "Found ($($links.length) total) links on the following page...`n'$($wr.ParsedHtml.nameProp)' - ($address)" -ForegroundColor Yellow
+            foreach($link in $links){
+                $props = @{
+                    WebPage=$wr.ParsedHtml.nameProp
+                    protocol=$link.protocol
+                    textContent=$link.textContent
+                    href=$link.href
+                }
+                $array += New-Object PSObject -Property $props
+            }
+            $array | Select-Object WebPage, protocol, textContent, href
+        }
+    }
+}
+
+
